acb8adf1f3fa6791c1ea638d91f6274f9491693e
Ben <ben.crommen@gmail.com>
2020-06-18 18:47:32 +0200

    home

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -3,15 +3,13 @@ import React from "react";
 const HomePage = (props) => {
   return (
     <div className="jumbotron">
-      <h1 className="display-3">Hello, world!</h1>
+      <h1 className="display-3">Bienvenue</h1>
       <p className="lead">
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
+        Pour débuter, créez vous un compte sur l'onglet inscription en haut à droite.
       </p>
       <hr className="my-4" />
       <p>
-        It uses utility classNamees for typography and spacing to space content
-        out within the larger container.
+        FreeCRM vous permet de gérer vos clients et les factures qui leurs sont liés.
       </p>
       <p className="lead">
         <a className="btn btn-primary btn-lg" href="#" role="button">
